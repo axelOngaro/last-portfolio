b51de7d75e150e8c343ed123cc8a98ceb9a3fbe5
Axel Ongaro <axel.ongaro.pro@gmail.com>
2021-06-24 18:35:12 +0200

    Layed out skills page with animations

--- pages/skills.js
+++ pages/skills.js
@@ -2,9 +2,31 @@ import Header from '../components/Header';
 
 export default function Skills() {
   return (
-    <div className="document">
-      <Header />
-      <h1>Skills page</h1>
+    <div className="">
+      <div className="header__wrapper">
+        <Header className="document" />
+      </div>
+      <main className="main__skills">
+        <section className="main__skills__technical">
+          <div className="main__skills__technical__field frontend">
+            <p>React + Vue</p>
+            <p>Next + Nuxt</p>
+            <p>Sass + Scss</p>
+          </div>
+          <div className="main__skills__technical__field backend" id="backend">
+            <p>Node + express</p>
+            <p>Prismic + Strapi</p>
+          </div>
+          <div className="main__skills__technical__field languages">
+            <p>Javascript + Typescript</p>
+            <p>Python</p>
+          </div>
+          <div className="main__skills__technical__field devops">
+            <p>Docker</p>
+            <p>AWS</p>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
