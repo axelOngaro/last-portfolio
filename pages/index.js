d0e505cba09d9ff43bfef59326b3530e16e0047a
Axel Ongaro <axel.ongaro.pro@gmail.com>
2021-06-24 17:01:26 +0200

    Styled landing page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,27 @@
 import Head from 'next/head';
 import Header from '../components/Header';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div className="document">
       <Header />
-      <p className="title">Coucou perruche</p>
+      <main className="home__main">
+        <div className="home__text">
+          <p className="home__text__intro">
+            Creative and Technical skills to solve your problems
+          </p>
+          <p className="home__text__presentation">
+            Designer and developer, based in Toulouse, France.
+          </p>
+          <Link href="/contact">
+            <a>
+              <button className="home__button">HIRE ME</button>
+            </a>
+          </Link>
+        </div>
+        <div className="home__illustration"></div>
+      </main>
     </div>
   );
 }
