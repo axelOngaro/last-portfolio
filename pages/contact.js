245db2133ff08e75119a7c586d663181ca87ee80
Axel Ongaro <axel.ongaro.pro@gmail.com>
2021-06-24 17:20:16 +0200

    styled contact page

--- pages/contact.js
+++ pages/contact.js
@@ -2,9 +2,25 @@ import Header from '../components/Header';
 
 export default function Contact() {
   return (
-    <div className="document">
+    <div className="document contact__page">
       <Header />
-      <h1>Contact page</h1>
+      <main className="contact__main">
+        <p className="contact__mail">axel.ongaro.pro@gmail.com</p>
+        <ul className="contact__list">
+          <a href="https://www.google.com" target="_blank">
+            <li className="contact__list__item">GITHUB</li>
+          </a>
+          <a href="https://www.google.com" target="_blank">
+            <li className="contact__list__item">LINKEDIN</li>
+          </a>
+          <a href="https://www.google.com" target="_blank">
+            <li className="contact__list__item">TWITTER</li>
+          </a>
+          <a href="https://www.google.com" target="_blank">
+            <li className="contact__list__item">MEDIUM</li>
+          </a>
+        </ul>
+      </main>
     </div>
   );
 }
